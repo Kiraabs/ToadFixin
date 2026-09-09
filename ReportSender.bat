91ecbdf4ade5792f4953d5e8bc3c29066356482b
Kira <kirayoung1007@gmail.com>
2026-09-09 19:37:36 +0300

    Изменены Updater.bat и ReportSender.bat

--- ReportSender.bat
+++ ReportSender.bat
@@ -1,19 +1,9 @@
-﻿@echo off
+@echo off
 cd /d "%~dp0"
-
 echo Sending report...
-
 git add Bugreport.*
 git commit
 git push -ff
-
-if errorlevel 1 (
-    echo.
-    echo Sending error!
-    pause
-    exit /b 1
-)
-
 echo.
 echo Sending done.
 pause
--- Updater.bat
+++ Updater.bat
@@ -1,23 +1,7 @@
-﻿@echo off
+@echo off
 cd /d "%~dp0"
-
-echo Terminating ToadFixin...
 taskkill /F /IM ToadFixin.exe
-
 timeout /t 1 /nobreak >nul
-
-echo.
-echo Updating...
 git pull --ff-only
-
-if errorlevel 1 (
-    echo.
-    echo Updating error!
-    pause
-    exit /b 1
-)
-
-echo.
-echo Updating done.
 pause
 start "" "%~dp0ToadFixin.exe"
